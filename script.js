cfa00dc633479d1a7407daf4c340ed926aa63ff7
yanitsa786 <57063886+yanitsa786@users.noreply.github.com>
2022-03-12 11:23:42 +0200

    second commit

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ function remove() {
   var listItem = document.querySelector('li:last-child');
   listItem.parentElement.removeChild(listItem);
 }
+
+function add() {
+  var listItem = document.querySelector('li:last-child');
+  listItem.parentElement.addChild(listItem);
+}
